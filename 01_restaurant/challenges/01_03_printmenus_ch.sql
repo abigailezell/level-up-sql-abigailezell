d193a98144361a675b3454f4544d1ac6b89c84bf
abigailezell <abigail.ezell@oracle.com>
2024-08-06 17:05:24 +0000

    r

--- 01_restaurant/challenges/01_03_printmenus_ch.sql
+++ 01_restaurant/challenges/01_03_printmenus_ch.sql
@@ -1,5 +1,13 @@
 -- Create reports that will be used to make three menus.
 
 -- Create a report with all the items sorted by price (lowest to highest).
+select * from Dishes
+order by Price;
 -- Create a report showing appetizers and beverages.
+select * from Dishes
+where Type in ('Appetizer','Beverage')
+order by Type
 -- Create a report with all items except beverages.
+select * from Dishes 
+where Type is not 'Beverage'
+order by Type
