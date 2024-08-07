0a9c0c5271bc367cb2f6a77ed60b4ddaf9f1e87a
abigailezell <abigail.ezell@oracle.com>
2024-08-07 18:01:24 +0000

    k

--- 02_library/challenges/02_01_checkavailability_ch.sql
+++ 02_library/challenges/02_01_checkavailability_ch.sql
@@ -1,2 +1,20 @@
 -- Determine how many copies of the book 'Dracula'
 -- are available for library patrons to borrow.
+select count(*)
+from books
+where title='Dracula';
+
+select count(*)
+from books join loans
+on books.bookid = loans.bookid
+where books.title = 'Dracula' and loans.returneddate is NULL;
+
+select 
+(select count(*)
+from books
+where title='Dracula') - 
+(select count(*)
+from books join loans
+on books.bookid = loans.bookid
+where books.title = 'Dracula' and loans.returneddate is NULL) 
+as availablecopies;
